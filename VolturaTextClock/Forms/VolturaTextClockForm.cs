22175b3decf2cd3d98cc1dded0117c552de918a6
Joakim Skoglund <joakim@voltura.se>
2020-12-31 14:33:03 +0100

    Fix bug

--- VolturaTextClock/Forms/VolturaTextClockForm.cs
+++ VolturaTextClock/Forms/VolturaTextClockForm.cs
@@ -115,9 +115,9 @@ namespace VolturaTextClock
             return path.Replace('\\', '/');
         }
 
-        private void UpdateClockText()
+        private void UpdateClockText(bool force = false)
         {
-            if (Visible && (m_SettingsForm == null || m_SettingsForm.Visible == false) && TextClock.GetImage(m_Theme))
+            if (Visible && (m_SettingsForm == null || m_SettingsForm.Visible == false) && ((TextClock.GetImage(m_Theme) == true) || (force == true)))
             {
                 clockPicBox?.Image?.Dispose();
                 clockPicBox.Image = LoadBitmapUnlocked(m_Theme.ClockImageFullPath);
@@ -157,7 +157,7 @@ namespace VolturaTextClock
             BackgroundImage = Properties.Resources.background;
             BackgroundImageLayout = ImageLayout.Zoom;
             ImageOverlay.SetGCSettings();
-            UpdateClockText();
+            UpdateClockText(true);
         }
 
         private void OptionsPicBox_Click(object sender, EventArgs e)
